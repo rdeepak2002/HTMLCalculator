9058ed81b75cd2cbc7a399c8db9f110e5a7c8ee4
Deepak Ramalingam <rdeepak2002@gmail.com>
2019-07-01 21:17:11 +0530

    fixed calculation issue

--- script.js
+++ script.js
@@ -8,55 +8,6 @@ function clr() {
 }
 
 function calculate() {
-	expression = document.getElementById("input").value + "&"
-
-	operations = ["+", "-", "/", "*"]
-	expArr = []
-	curNum = ""
-
-	for(var i = 0; i < expression.length; i++) {
-		character = expression.charAt(i)
-
-	  if(operations.includes(character) || character == "&") {
-	  	if(curNum.length > 0) {
-		  	expArr.push(parseInt(curNum))
-		  	curNum = ""
-	  	}
-	  	if(character != "&") {
-		  	expArr.push(character)
-	  	}
-	  }
-	  else {
-	  	curNum = curNum + character
-	  }
-	}
-
-	while(expArr.length > 1) {
-		while(expArr.includes("*")) {
-			var index = expArr.indexOf("*")
-			expArr[index] = expArr[index-1] * expArr[index+1]
-			expArr.splice(index-1, 1);
-			expArr.splice(index, 1);
-		}
-		while(expArr.includes("/")) {
-			var index = expArr.indexOf("/")
-			expArr[index] = expArr[index-1] / expArr[index+1]
-			expArr.splice(index-1, 1);
-			expArr.splice(index, 1);
-		}
-		while(expArr.includes("+")) {
-			var index = expArr.indexOf("+")
-			expArr[index] = expArr[index-1] + expArr[index+1]
-			expArr.splice(index-1, 1);
-			expArr.splice(index, 1);
-		}
-		while(expArr.includes("-")) {
-			var index = expArr.indexOf("-")
-			expArr[index] = expArr[index-1] - expArr[index+1]
-			expArr.splice(index-1, 1);
-			expArr.splice(index, 1);
-		}
-	}
-
-	document.getElementById("input").value = expArr[0]
+	expression = document.getElementById("input").value
+	document.getElementById("input").value = eval(expression)
 }
